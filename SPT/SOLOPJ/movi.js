7a557c7599015ca7fa0c4abb01d649459ad4e678
byeongtaek <respect21047463@gmail.com>
2023-10-22 22:34:34 +0900

    fianl

--- SPT/SOLOPJ/movi.js
+++ SPT/SOLOPJ/movi.js
@@ -1,15 +1,11 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDRlODIzZDI1NWI3MGEzZmYyYmQ1YTY4OTM0ODAxNSIsInN1YiI6IjY1MmVhNjJlYTgwMjM2MDBlMGFjNWM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oLDzypYiWDVmVxaWuZsOWHjypvnuR6Jrm-rZdI07sLw'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDRlODIzZDI1NWI3MGEzZmYyYmQ1YTY4OTM0ODAxNSIsInN1YiI6IjY1MmVhNjJlYTgwMjM2MDBlMGFjNWM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oLDzypYiWDVmVxaWuZsOWHjypvnuR6Jrm-rZdI07sLw",
+  },
 };
-
-//  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
 
 //페치 받아와서 json을  js 배열로 바꿔줌
 function sendFetchRequest(url) {
@@ -18,91 +14,53 @@ function sendFetchRequest(url) {
   });
 }
 // //해당 url의 내용을 js 배열로 바꾼 뒤 for반복문 돌려서 카드 생성
-async function fetchPost() {
+async function fetchPost(keyword = "") {
   const datas = await sendFetchRequest(
-    'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1'
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"
   );
-console.log(datas.results)
+
   const lists = datas.results;
-  let a = '';
-  for (const list of lists) {
-    // console.log(`https://image.tmdb.org/t/p/w300_and_h450_bestv2${list.poster_path}`)
- a += `      
- <ul id ="${list.id}">
- <h2>${ list.original_title.toUpperCase()}</h2>
- <img src='https://image.tmdb.org/t/p/w300_and_h450_bestv2${list.poster_path}'/> 
- <p>${list.overview}</p>
- <p>${list.vote_average}</p>
-</ul>
- `;
-document.querySelector('.card-list').innerHTML = a
-  }
+
+  /*;*/
+
+  let a = "";
+  // console.log(lists)
+  lists
+    .filter((obj) => {
+      const title = obj.title.toUpperCase();
+      const key = keyword.toUpperCase();
+      return title.includes(key);
+    })
+    .forEach((list) => {
+      a += `
+  <ul id ="${list.id}">
+  <h2>${list.original_title.toUpperCase()}</h2>
+  <img class='imgclick' data-id="${
+    list.id
+  }" src='https://image.tmdb.org/t/p/w300_and_h450_bestv2${list.poster_path}'/>
+  <p>${list.overview}</p>
+  <p>${list.vote_average}</p>
+  </ul>
+  `;
+    });
+
+  document.querySelector(".card-list").innerHTML = a;
+  //이미지 클릭시 팝업창 오픈
+
+  imgCl();
 }
+function imgCl() {
+  const imgClick = document.querySelectorAll(".imgclick");
+  imgClick.forEach(btn => {
+    btn.addEventListener("click", (event) => alert(event.target.dataset.id));
+  })
+}
+const $searForm = document.querySelector("#search_form");
+
+$searForm.addEventListener("submit", (event) => {
+  event.preventDefault(); //   ㄴ클릭이나 엔터라는 submit = 이벤트가 발생하면 처리하겠다.
+  const $searInput = document.querySelector("#search-input");
+  fetchPost($searInput.value);
+});
+
 fetchPost();
-
-// // 검색 영역
-
-// const moives = [];
-
-// const renderMovies = (filter = '') => {
-//   const movieList = document.getElementById('movie-list');
-
-// //     if (movies.length === 0) {
-// //     movieList.classList.remove('visible');
-// //     return;
-// //   } else {
-// //     movieList.classList.add('visible');
-// //   }
-//   movieList.innerHTML = '';
-
-//   const filterMovies = !filter
-//     ? movies
-//     : movies.filter((movie) => movie.info.title.includes(filter));
-
-//     filterMovies.forEach((movie) => { 
-//     const movieEl = document.createElement('li');
-//     let text = movie.info.title + `-`;
-//     for (const key in movie.info){
-//         if ( key !== 'title')
-//         text += `${key}: ${movie.info[key]}`;
-//     }
-//     movieEl.textContent = text; 
-//     movieList.append(movieEl);
-//   });
-// };
-
-// const addMovieHandler = () => {
-//     const title = document.getElementById('title').value;
-//     const extraName = document.getElementById('extra-name').value;
-//     const extraValue = document.getElementById('extra-value').value;
-  
-//     if (
-//       title.trim() === '' ||
-//       extraName.trim() === '' ||
-//       extraValue.trim() === ''
-//     ) {
-//       return;
-//     }
-  
-//     const newMovie = {
-//       info: {
-//         title,
-//         [extraName]: extraValue
-//       },
-//       id: Math.random()
-//     };
-  
-//     movies.push(newMovie);
-//     renderMovies();
-//   };
-
-
-// const searchMovieHandler = () => {
-//   const filterTerm = document.getElementById("search-input").value;
-//   renderMovies(filterTerm);
-// };
-
-
-
-// const searchBtn = document.getElementById("serach-btn");
-// searchBtn.addEventListener("click", searchMovieHandler);
