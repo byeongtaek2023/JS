69590b82c0f18d574beee756bf32bf41665a8602
byeongtaek <respect21047463@gmail.com>
2023-11-16 11:23:04 +0900

    algo 추가

---
+++ SPT/algo/두수의곱.js
@@ -0,0 +1,4 @@
+function solution(num1, num2) {
+    const multi = num1 * num2
+     return multi;
+ }
---
+++ SPT/algo/두수의차.js
@@ -0,0 +1,5 @@
+function solution(num1, num2) {
+    const subtraction = num1 - num2;
+
+    return subtraction;
+}
